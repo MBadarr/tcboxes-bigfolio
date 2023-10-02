d827a32785524ae440c68df49b81fc2341171e9c
MBadarr <mbadar418@gmail.com>
2023-10-02 22:26:47 +0500

    Color Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                text: ['Inter Tight', 'sans-serif',]
+                text: ['Inter+Tight', 'sans-serif',]
             },
             colors: {
                 'bg-1': '#0A0A0C',
                 'bg-2': '#FFF',
                 'bg-3': '#EFE9E0',
                 'bg-4': '#8A8A8A',
+                'bg-5': '#0A0A0C',
                 'bg-button': '#1FB895',
             },
         },
